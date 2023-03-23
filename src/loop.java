d44c50ea65267ab3580c588499c5ecc67fe864d1
Aman Kumar <amankumardagur7878@gmail.com>
2023-03-23 14:45:00 +0530

    IncrementDecrement

--- src/loop.java
+++ src/loop.java
@@ -16,9 +16,36 @@ public class loop
 //        for (int i = 1; i <11 ; i++) {
 //            System.out.println(n +"X"+ i +" = "+n*i);
 //        }
-        for (int i = 10; i >0 ; i--) {
-           System.out.println(n +"X"+ i +" = "+n*i);
-    }
+//        for (int i = 10; i >0 ; i--) {
+//           System.out.println(n +"X"+ i +" = "+n*i);
+//    }
+//        for (char ch = 'a'; ch <= 'z' ; ch++) {
+//        System.out.println(ch);
+//        }
+//        for (int i = 1; i <=100 ; i++) {
+//            if(i%2!=0){
+//                System.out.print(i+" ");
+//            }
+//        }
+//        int sum=0;
+//        for (int i = 1; i <=n; i++) {
+//            sum+=i;
+//        }
+//        System.out.println(sum);
+//        int sum=0;
+//        for (int i = 1; i <=n; i++) {
+//            if(i%2==0)sum+=i;
+//        }
+//        System.out.println(sum);
+//        for (char i = 0; i <=255 ; i++) {
+//            System.out.print(i+" ");
+//        }
+//        int fact=1;
+//        for (int i = 1; i <=n; i++) {
+//            fact=fact*i;
+//        }
+//        System.out.println(fact);
+
 
     }
 }
