abc3a6e8228194d56bc3068905ff47c05f7b0c01
Aman Kumar <amankumardagur7878@gmail.com>
2023-03-23 14:24:24 +0530

    IncrementDecrement

---
+++ src/IncrementDecrement.java
@@ -0,0 +1,55 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        /*int i=11;
+        i =i++ + ++i;
+        System.out.println(i);*/
+
+//        int a=11,b=22,c;
+//        c=a+b+a++ +b++ + ++a + ++b;
+//        System.out.println("a="+a);
+//        System.out.println("b="+b);
+//        System.out.println("c="+c);
+
+//        int i=0;
+//        i=i++ - --i + ++i - i--;
+//        System.out.println(i);
+
+//        int i=1,j=2,k=3;
+//        int m =i-- - j-- - k--;
+//        System.out.println("i="+i);
+//        System.out.println("j="+j);
+//        System.out.println("k="+k);
+//        System.out.println("m="+m);
+
+//        int a=1,b=2;
+//        System.out.println(--b - ++a + ++b - --a);
+
+//        int i =11;
+//        int j= --(i++);
+//
+//        int m=0,n=0;
+//        int p=--m* --n* n--*m--;
+//        System.out.println(p);
+
+//        int a=1;
+//        a=a++ + ++a * --a -a--;
+//        System.out.println(a);
+
+//        int m=1010, n=1010;
+//        System.out.println(m++ / ++n * n--/--m);
+
+//        int x=001,y=010,z=100;
+//        int i=--x +y++ -z-- - --z + ++y - --x + y-- - --x;
+//        System.out.println("x="+x);
+//        System.out.println("y="+x);
+//        System.out.println("z="+y);
+//        System.out.println("i="+z);
+
+//        char ch='A';
+//        System.out.println(++ch);
+
+//        char ch="123".charAt(1);
+//        System.out.println(ch++ + ++ch);
+
+    }
+}
---
+++ src/loop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class loop
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+//        for (int i = 1; i <=n ; i++) {
+//            System.out.println(i);
+//        }
+//        for(int i = n; i >0 ; i--) {
+//
+//           System.out.println(i);
+//        }
+//        for (int i = 1; i <11 ; i++) {
+//            System.out.println(n +"X"+ i +" = "+n*i);
+//        }
+        for (int i = 10; i >0 ; i--) {
+           System.out.println(n +"X"+ i +" = "+n*i);
+    }
+
+    }
+}
